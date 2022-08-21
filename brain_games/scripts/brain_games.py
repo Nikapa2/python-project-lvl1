f0d4e602dd98a1a7ccff2f39e6dbfdbecd23a8ec
Anton Nikitin <palich.nik82@gmail.com>
2022-08-21 22:26:44 +0300

    .

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 
 
-def greet():
-    return (print('Welcome to the Brain Games!'))
-
-
 def main():
-    greet()
-
+    print('Welcome to the Brain Games!')
 
 if __name__ == '__main__':
     main()
