31d373ac9a364fad2f66e705ecdbd8a9d9134438
Anton Nikitin <palich.nik82@gmail.com>
2022-08-22 23:29:01 +0300

    .

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,32 +1,26 @@
 import prompt
 
 
-WIN_SCORE = 3
-
-
-def welcome_user():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
+NUMBER_OF_ROUNDS = 3
 
 
 def engine(game):  # game:name game modul.
-    name = welcome_user()
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
     print(game.GAME_RULES)
-    for i in range(WIN_SCORE):
+    for _ in range(NUMBER_OF_ROUNDS):
         task, correct_answer = game.process_of_game()
         print("Question: {}".format(task))
         user_answer = prompt.string("Your answer: ")
         if user_answer == correct_answer:
             print('Correct!')
             continue
-        fail_answer(name, correct_answer, user_answer)
+        print("'{0}' is wrong answer ;(. Correct answer was '{1}'.".format(user_answer, correct_answer))  # noqa: E501
+        print("Let's try again, {0}!".format(name))
         break
     else:
         print(f'Congratulations, {name}!')
 
 
-def fail_answer(name, correct_answer, user_answer):
-    print("'{0}' is wrong answer ;(. Correct answer was '{1}'.".format(user_answer, correct_answer))  # noqa: E501
-    print("Let's try again, {0}!".format(name))
+
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from brain_games.engene import engine
+from brain_games.engine import engine
 from brain_games.games import prime_number
 
 
